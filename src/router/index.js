7826ff10819f922fbe94138227b0561bbf35f86f
jack.lei <threetowns@163.com>
2018-06-10 16:55:30 +0800

    [router] Mod: config router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+const dashboard = r => require.ensure([], () => r(require('../pages/dashboard/index')), 'dashboard')
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+const routes = [
+  {
+    path: '/',
+    name: 'dashboard',
+    component: dashboard,
+    meta: {
+      title: '管理平台'
     }
-  ]
+  }
+];
+
+const router = new Router({
+  mode: 'history',
+  linkActiveClass: 'active',
+  routes
 })
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title ? to.meta.title : '能力开放平台'
+  next()
+})
+
+export default router;
