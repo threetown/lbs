5b3d37771a2f876875f5ed42e196ec833c4b3434
jack.lei <threetowns@163.com>
2018-06-11 09:19:57 +0800

    [pages application] Init: application tpl.

---
+++ src/pages/application/layout.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div class="m-25 mt-65">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
---
+++ src/pages/application/recyclebin.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        recyclebin
+    </div>
+</template>
