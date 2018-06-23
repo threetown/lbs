7e53656a3d7e3e2b643a6e2df37844ac5ce05ccc
leon <threetowns@163.com>
2018-06-23 11:13:56 +0800

    [echarts] Add: echarts config.

---
+++ src/config/echartsConfig.js
@@ -0,0 +1,93 @@
+import echarts from "echarts";
+
+const echartsConfig = {
+    itemColor: ["#0087f1", "#00A85B"],
+    labelColor: ['#999'],
+    lineColor: ['#e7e7ee'],
+    areaColor: ['#fafafa'],
+    resize(obj) {
+        window.addEventListener('resize', function () {
+            obj.resize()
+        }.bind(this))
+    },
+    lineChartOptions(xAxisData, seriesData){
+        let option = {
+            tooltip: {
+                trigger: 'axis',
+                backgroundColor: '#fff',
+                textStyle: { color: '#5dbef6' },
+                axisPointer: { lineStyle: { color: '#e7e7ee' }},
+                extraCssText: 'box-shadow: 0 0 5px rgba(0,0,0,0.3)'
+            },
+            grid: {
+                left: '0',
+                right: '0',
+                bottom: '0',
+                top: '6%',
+                containLabel: true
+            },
+            xAxis : [
+                {
+                    type : 'category',
+                    boundaryGap : false,
+                    splitLine: {
+                        show: true,
+                        interval: 'auto',
+                        lineStyle: {
+                            color: this.lineColor
+                        }
+                    },
+                    axisTick: {
+                        show: false
+                    },
+                    axisLine: {
+                        lineStyle: {
+                            color: this.lineColor
+                        }
+                    },
+                    axisLabel: { textStyle: { color: '#999' } },
+                    data:  xAxisData ? xAxisData : ['周一','周二','周三','周四','周五','周六','周日']
+                }
+            ],
+            yAxis : [
+                {
+                    type : 'value',
+                    splitLine: { lineStyle: { color: this.lineColor } },
+                    axisTick: { show: false },
+                    axisLine: { lineStyle: { color: this.lineColor } },
+                    axisLabel: { textStyle: { color:  this.labelColor} },
+                    gridIndex: 0,
+                    splitArea: { show: true, areaStyle: { color: this.areaColor }}
+                }
+            ],
+            series : [
+                {
+                    name:'邮件营销',
+                    type:'line',
+                    stack: '总量',
+                    areaStyle: {
+                        normal: {
+                            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                                offset: 0,
+                                color: 'rgba(150, 201, 254,0.5)'
+                            }, {
+                                offset: 1,
+                                color: 'rgba(150, 201, 254,0.2)'
+                            }], false)
+                        }
+                    },
+                    itemStyle: {
+                        normal: {
+                            color: this.itemColor[0]
+                        }
+                    },
+                    data: seriesData ? seriesData : [120, 132, 101, 134, 90, 230, 210]
+                }
+            ]
+        };
+        
+        return option;
+    }
+}
+
+export default echartsConfig;
