44c220243fc8f026a00e692aa61638b24109d6da
leon <threetowns@163.com>
2018-07-07 21:30:23 +0800

    [auth] Mod: better load auth.

--- src/util/auth/index.js
+++ src/util/auth/index.js
@@ -16,22 +16,39 @@ auth.initRouter = function (vm) {
         component: 'error/404'
     }];
     // 模拟异步请求
+    vm.$Loading.start();
+    let gloabLoading = vm.$Message.loading({
+        content: '努力加载中，请稍等...',
+        duration: 0
+    })
     // fetch({ url: '/static/mock/data/menu.json', method: 'get' }).then(res => {
     fetch({ url: '/center/user/getAmpAuthByCondition', method: 'post' }).then(res => {
-        let data = res.data;
-        auth.initRouterNode(constRoutes, data.allAmpAuth);
-        auth.initRouterNode(otherRoutes, otherRouter);
-        // 添加主界面路由
-        vm.$store.commit(types.AUTH_APP_ROUTER, constRoutes.filter(item => item.children.length > 0));
-        // 添加全局路由
-        vm.$store.commit(types.AUTH_DEFAULT_ROUTER, otherRoutes);
-        // 刷新界面菜单
-        vm.$store.commit(types.AUTH_MENU_LIST, constRoutes.filter(item => item.children.length > 0));
+        if(res.state === 0){
+            let data = res.data;
+            auth.initRouterNode(constRoutes, data.allAmpAuth);
+            auth.initRouterNode(otherRoutes, otherRouter);
+            // 添加主界面路由
+            vm.$store.commit(types.AUTH_APP_ROUTER, constRoutes.filter(item => item.children.length > 0));
+            // 添加全局路由
+            vm.$store.commit(types.AUTH_DEFAULT_ROUTER, otherRoutes);
+            // 刷新界面菜单
+            vm.$store.commit(types.AUTH_MENU_LIST, constRoutes.filter(item => item.children.length > 0));
 
-        // 顶部-消息数和昵称
-        vm.$store.commit(types.AUTH_USER_NICKNAME, data.staffName)
-        vm.$store.commit(types.AUTH_USER_MESSAGE, data.unReadNotice)
-
+            // 顶部-消息数和昵称
+            vm.$store.commit(types.AUTH_USER_NICKNAME, data.staffName)
+            vm.$store.commit(types.AUTH_USER_MESSAGE, data.unReadNotice)
+            vm.$Loading.finish()
+        }else{
+            vm.$Loading.error();
+            vm.$Message.error(res.message)
+        }
+        setTimeout(gloabLoading, 10)
+    }).catch(err =>{
+        vm.$Loading.error()
+        setTimeout(gloabLoading, 10)
+        setTimeout(function(){
+            window.location = 'http://testsso.cindata.cn/sso/login?callbackUrl=http://testlbs.cindata.cn&p=gxdyun'
+        },100)
     });
 };
 
