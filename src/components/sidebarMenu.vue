f00b019ba99dd7658fdc7aba5dbb353eac9896ea
leon <threetowns@163.com>
2018-07-07 11:24:36 +0800

    [auth] Mod: sider bar menu component.

--- src/components/sidebarMenu.vue
+++ src/components/sidebarMenu.vue
@@ -1,90 +1,42 @@
 <template>
-    <Menu :active-name="active_name" :theme="theme" :open-names="open_names"  @on-select="changeMenu" width="200px">
-        <Submenu name="application">
-            <template slot="title">
-                <Icon type="ios-paper"></Icon>
-                应用管理
-            </template>
-            <MenuItem name="apps">我的应用</MenuItem>
-            <MenuItem name="recyclebin" key="recyclebin">回收站</MenuItem>
-        </Submenu>
-        <Submenu name="data">
-            <template slot="title">
-                <Icon type="ios-people"></Icon>
-                数据可视化
-            </template>
-            <MenuItem name="project">可视化项目</MenuItem>
-            <MenuItem name="mydata">我的数据</MenuItem>
-        </Submenu>
-        <Submenu name="sys">
-            <template slot="title">
-                <Icon type="stats-bars"></Icon>
-                系统监控
-            </template>
-            <MenuItem name="bigdata">大屏监控</MenuItem>
-            <MenuItem name="scree">数据总览</MenuItem>
-            <MenuItem name="business">业务管理</MenuItem>
-            <MenuItem name="service">服务管理</MenuItem>
-            <MenuItem name="database">数据库连接池</MenuItem>
-            <MenuItem name="track">追踪服务</MenuItem>
-            <MenuItem name="commit">操作日志</MenuItem>
-        </Submenu>
-        <Submenu name="user">
-            <template slot="title">
-                <Icon type="ios-people"></Icon>
-                用户管理
-            </template>
-            <MenuItem name="list">用户列表</MenuItem>
-            <MenuItem name="log">登陆日志</MenuItem>
-        </Submenu>
-        <Submenu name="personal">
-            <template slot="title">
-                <Icon type="ios-people"></Icon>
-                个人中心
-            </template>
-            <MenuItem name="info">帐号信息</MenuItem>
-            <MenuItem name="quota">配额管理</MenuItem>
-            <MenuItem name="record">配额申请记录</MenuItem>
-        </Submenu>
-        <Submenu name="msg">
-            <template slot="title">
-                <Icon type="ios-people"></Icon>
-                消息
-            </template>
-            <MenuItem name="index">消息</MenuItem>
-        </Submenu>
+    <Menu :active-name="activeName" :theme="theme" :open-names="openNames"  @on-select="changeMenu" width="200px">
+        <template v-for="item in MenuList">
+            <MenuItem v-if="item.children.length<=1" :name="item.children[0].name" :key="item.path">
+                <Icon :type="item.icon" :key="item.icon"></Icon> {{item.title}}
+            </MenuItem>
+            <Submenu v-if="item.children.length > 1" :name="item.name" :key="item.path">
+                <template slot="title">
+                    <Icon :type="item.icon"></Icon> {{item.title}}
+                </template>
+                <template v-for="child in item.children">
+                    <MenuItem :name="child.name" :key="child.name">{{ child.title }}</MenuItem>
+                </template>
+            </Submenu>
+        </template>
     </Menu>
 </template>
 
 <script>
     export default {
-    	name: 'sideBar',
-        data () {
-            return {
-                theme: 'dark',
-                open_names: []
+        name: 'sideBar',
+        props: {
+            MenuList:{
+                type: Array
+            },
+            theme: {
+                type: String,
+                default: 'dark'
+            },
+            activeName: {
+                type: String
+            },
+            openNames: {
+                type: Array
             }
         },
         methods: {
             changeMenu(active) {
-                this.$router.push({
-                    name: active
-                });
-            },
-            init(){
-                this.active_name = this.$route.name;
-                let relative_path_arr = this.$route.path.split('/');
-                this.open_names = relative_path_arr.length > 1 ?[relative_path_arr[1]] : ['application'];
-            }
-        },
-        created() {
-            this.init()
-        },
-        watch:{
-            '$route' (to, from){
-                if(to.name == 'quota'){
-                    this.open_names = [to.name];
-                }
+                this.$emit('on-change', active)
             }
         }
     }
