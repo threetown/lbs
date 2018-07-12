0d499cbea53eed3a6ada7b1b62e20a9795eb0e2f
leon <threetowns@163.com>
2018-07-12 11:47:22 +0800

    [echarts] Add: area line chart component.

---
+++ src/components/echarts/leon-area-line-chart.vue
@@ -0,0 +1,55 @@
+    <template>
+        <div class="line-chart" :id="id" :option="option" :style="style"></div>
+    </template>
+
+    <script>
+    import echarts from "echarts";
+    import echartsConfig from "src/config/echartsConfig";
+
+    export default {
+        name: "leon-line-chart",
+        props: {
+            id: {
+                type: String
+            },
+            height: {
+                type: String,
+                default: '300px'
+            },
+            width: {
+                type: String,
+                default: '100%'
+            },
+            option: {
+                type: Object,
+                default() {
+                    return {}
+                }
+            }
+        },
+        data: function() {
+            return {}
+        },
+        mounted() {
+            this.init()
+        },
+        methods: {
+            init() {
+                const self = this;
+                console.log(self.option, 42)
+                echartsConfig.init({
+                    id: self.id,
+                    option: echartsConfig.lineAreaChartOptions(self.option)
+                })
+            }
+        },
+        computed: {
+            style () {
+                return {
+                    height: this.height,
+                    width: this.width
+                }
+            }
+        }
+    };
+    </script>
