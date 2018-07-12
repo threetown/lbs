7dc0044e26c5e36986facbdc23a2d560883341da
leon <threetowns@163.com>
2018-07-12 11:44:49 +0800

    [echarts] Add: line area chart option.

--- src/config/echartsConfig.js
+++ src/config/echartsConfig.js
@@ -5,12 +5,21 @@ const echartsConfig = {
     labelColor: ['#999'],
     lineColor: ['#e7e7ee'],
     areaColor: ['#fafafa'],
+    color: ["#6b4ce4","#3c6be0","#52a5e5","#69d3e7"],
+    init(params){
+        let EChart = echarts.init(document.getElementById(params.id));
+        let option = params.option;
+        if (option && typeof option === "object") {
+            EChart.setOption(option, true);
+        }
+        echartsConfig.resize(EChart)
+    },
     resize(obj) {
         window.addEventListener('resize', function () {
             obj.resize()
         }.bind(this))
     },
-    lineAreaChartOptions(xAxisData, seriesData){
+    lineAreaChartOptions(params){
         let option = {
             tooltip: {
                 trigger: 'axis',
@@ -46,7 +55,7 @@ const echartsConfig = {
                         }
                     },
                     axisLabel: { textStyle: { color: this.labelColor } },
-                    data:  xAxisData ? xAxisData : ['周一','周二','周三','周四','周五','周六','周日']
+                    data: params && params.xAxisData ? params.xAxisData : []
                 }
             ],
             yAxis : [
@@ -62,9 +71,8 @@ const echartsConfig = {
             ],
             series : [
                 {
-                    name:'邮件营销',
+                    name: params && params.seriesData && params.seriesData.name ? params.seriesData.name : '总量',
                     type:'line',
-                    stack: '总量',
                     areaStyle: {
                         normal: {
                             color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
@@ -81,80 +89,13 @@ const echartsConfig = {
                             color: this.itemColor[0]
                         }
                     },
-                    data: seriesData ? seriesData : [120, 132, 101, 134, 90, 230, 210]
+                    data: params && params.seriesData ? params.seriesData : []
                 }
             ]
         };
         
         return option;
-    },
-    lineChartOptions(xAxisData, seriesData){
-        let option = {
-            tooltip: {
-                trigger: 'axis',
-                backgroundColor: '#fff',
-                textStyle: { color: '#5dbef6' },
-                axisPointer: { lineStyle: { color: '#e7e7ee' }},
-                extraCssText: 'box-shadow: 0 0 5px rgba(0,0,0,0.3)'
-            },
-            grid: {
-                left: '0',
-                right: '0',
-                bottom: '0',
-                top: '6%',
-                containLabel: true
-            },
-            xAxis: {
-                type : 'category',
-                boundaryGap : false,
-                splitLine: {
-                    show: true,
-                    interval: 'auto',
-                    lineStyle: {
-                        color: this.lineColor
-                    }
-                },
-                axisTick: {
-                    show: false
-                },
-                axisLine: {
-                    lineStyle: {
-                        color: this.lineColor
-                    }
-                },
-                axisLabel: { textStyle: { color: this.labelColor } },
-                data: ['周一','周二','周三','周四','周五','周六','周日']
-            },
-            yAxis : [
-                {
-                    type : 'value',
-                    splitLine: { lineStyle: { color: this.lineColor } },
-                    axisTick: { show: false },
-                    axisLine: { lineStyle: { color: this.lineColor } },
-                    axisLabel: { textStyle: { color:  this.labelColor} },
-                    gridIndex: 0,
-                    splitArea: { show: true, areaStyle: { color: this.areaColor }}
-                }
-            ],
-            color: echartsConfig.itemColor,
-            series: [
-                {
-                    name:'用户总数',
-                    type:'line',
-                    stack: '总量',
-                    data:[220, 182, 191, 234, 290, 330, 310]
-                },
-                {
-                    name:'新增用户',
-                    type:'line',
-                    stack: '总量',
-                    data: [120, 132, 101, 134, 90, 230, 210]
-                }
-            ]
-        };
-        
-        return option;
-    },
+    }
 }
 
 export default echartsConfig;
