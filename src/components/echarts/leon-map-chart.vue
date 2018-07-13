b0c2e2bd879ebcdc1328eed7c3b2cef5074c91b8
leon <threetowns@163.com>
2018-07-13 14:32:52 +0800

    [echarts] Add: map chart component.

---
+++ src/components/echarts/leon-map-chart.vue
@@ -0,0 +1,62 @@
+    <template>
+        <div class="map-chart" :id="id" :option="option" :style="style"></div>
+    </template>
+
+    <script>
+    import echartsConfig from "src/config/echartsConfig";
+    require('echarts/map/js/china.js')
+
+    export default {
+        name: "leon-map-chart",
+        props: {
+            id: {
+                type: String
+            },
+            height: {
+                type: String,
+                default: '658px'
+            },
+            width: {
+                type: String,
+                default: '858px'
+            },
+            option: {
+                type: Object,
+                default() {
+                    return {}
+                }
+            }
+        },
+        data: function() {
+            return {}
+        },
+        mounted() {
+            this.init()
+        },
+        methods: {
+            init() {
+                const self = this;
+                echartsConfig.init({
+                    id: self.id,
+                    option: echartsConfig.MapEChartsOptions(self.option)
+                })
+            }
+        },
+        computed: {
+            style () {
+                return {
+                    height: this.height,
+                    width: this.width
+                }
+            }
+        },
+        watch: {
+            option: {
+                handler() {
+                    this.init();
+                },
+                deep: true
+            }
+        }
+    };
+    </script>
