2953170605f12472a4dfaaa6eb381c8d7b655052
leon <threetowns@163.com>
2018-07-25 14:06:34 +0800

    [personal record] Mod: record page.

--- src/pages/personal/record.vue
+++ src/pages/personal/record.vue
@@ -8,9 +8,13 @@
                 <Select size="large" v-model="search.currentType" @on-change="changeType" style="width:200px">
                     <Option v-for="item in search.statusType" :value="item.value" :key="item.value">{{ item.label }}</Option>
                 </Select>
-                <Select size="large" v-model="search.currentDate" style="width:200px">
+                <!-- <Select size="large" v-model="search.currentDate" style="width:200px">
                     <Option v-for="item in search.dateType" :value="item.value" :key="item.value">{{ item.label }}</Option>
-                </Select>
+                </Select> -->
+                <DatePicker size="large" type="datetimerange" placeholder="请选择查询时间" style="width: 350px;"
+                    @on-ok="triggerSearchDate"
+                    @on-change="selectDate"
+                    ></DatePicker>
             </Col>
         </Row>
         <div v-if="record.loading" :class="'Placeholder ' + record.state">{{record.loadTips}}</div>
@@ -34,35 +38,33 @@
         data () {
             return {
                 search: {
-                    currentType: '0',
+                    currentType: '',
                     statusType: [
                         { label: '全部申请', value: '' },
-                        { label: '已通过', value: '1' },
-                        { label: '未通过', value: '-1' }
+                        { label: '未审批', value: '0' },
+                        { label: '审批通过', value: '1' },
+                        { label: '未通过', value: '2' }
                     ],
-                    currentDate: '0',
-                    dateType: [
-                        { label: '全部申请', value: '0' },
-                        { label: '近三个月申请', value: '3' },
-                        { label: '近六个月申请', value: '6' },
-                        { label: '近一年申请', value: '12' },
-                    ]
+                    dataRange: []
                 },
                 recordColumns: [
-                    { title: 'Key信息', key: 'name', align: 'center' },
-                    { title: '接口信息', key: 'info', align: 'center' },
-                    { title: '提升类型', key: 'type', align: 'center' },
-                    { title: '当前配额', key: 'price', align: 'center' },
-                    { title: '申请提升至', key: 'upPrice', align: 'center' },
-                    { title: '状态', key: 'status', align: 'center', render: (h, params) => {
+                    { title: 'Key信息', key: 'keyName', align: 'center' },
+                    { title: '接口信息', key: 'serviceName', align: 'center' },
+                    { title: '提升类型', key: 'limittype', align: 'center', width: 95 },
+                    { title: '当前配额', key: 'currentLimit', align: 'center' },
+                    { title: '申请提升至', key: 'newLimit', align: 'center' },
+                    { title: '申请时间', key: 'createdDt', align: 'center' },
+                    { title: '状态', key: 'aprovalStatusCd', align: 'center', width: 96, render: (h, params) => {
                             let texts = '';
                             let classname = '';
-                            if(params.row.status === '未通过'){
+                            if(params.row.aprovalStatusCd === '未通过'){
                                 classname = 'status-error'
+                            }else if(params.row.aprovalStatusCd === '未审批'){
+                                classname = 'status-waiting'
                             }else{
                                 classname = 'status-success'
                             }
-                            texts = params.row.status;
+                            texts = params.row.aprovalStatusCd;
                             return h('div',{},[
                                 h('span', {
                                     class: classname,
@@ -70,7 +72,8 @@
                             ])
                         }
                     },
-                    { title: '反馈信息', key: 'desc', align: 'center' }
+                    { title: '备注', key: 'remark', align: 'center' },
+                    { title: '审核意见', key: 'aprovalRemark', align: 'center' }
                 ],
                 record:{
                     data: [],
@@ -92,13 +95,24 @@
                 this.record.page = v;
                 this.getList()
             },
-            getList(){
+            selectDate(v){
+                this.search.dataRange = v;
+            },
+            triggerSearchDate(){
+                const self = this;
+                this.getList({
+                    startTime: self.search.dataRange[0],
+                    endTime: self.search.dataRange[1]
+                })
+            },
+            getList(params){
                 const self = this;
                 let data = {
                     statusCd: this.search.currentType,
                     page: this.record.page,
                     rows: this.record.rows
                 }
+                data = Object.assign(data, params)
                 this.record.loading = true;
                 this.record.loadTips = '努力加载中，请稍等...'
                 this.record.state = 'loading';
@@ -106,7 +120,7 @@
                     if(res.state === 0){
                         let result = res.data;
                         if(result && result.data && result.data.length){
-                            self.record.data = tools.getQuotaRecord(result.data);
+                            self.record.data = result.data;
                             self.record.total = result.total;
                             self.record.loading = false;
                         }else{
